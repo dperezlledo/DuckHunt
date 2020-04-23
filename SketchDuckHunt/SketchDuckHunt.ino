31a2cf9169332aa60bcdd93bf54cb33203b9d2b1
dperezlledo <dperez.comercio@gmail.com>
2020-04-23 23:52:32 +0200

    Comunicacion Arduino y Java

--- SketchDuckHunt/SketchDuckHunt.ino
+++ SketchDuckHunt/SketchDuckHunt.ino
@@ -8,53 +8,69 @@ void setup() {
  pinMode(D12, INPUT);
 }
 
-int c=0, max, min, sw=1, value, calibracion=1, pulsador;
+int c=0, max, min,  value, pulsador;
 int MIN_BLACK, MAX_BLACK;
 int MIN_WHITE, MAX_WHITE; 
-
-void loop() {
-  while (calibracion==1) {
-    Serial.println("CALIBRANDO COLORES NEGRO Y BLANCO");
-    Serial.print("Posicionese en color NEGRO (10 seg de espera) ");
-    esperar(10);
-    calibrar();
-    MIN_BLACK= min;
-    MAX_BLACK=max;
-    sw=1;
-    Serial.print("Posicionese en color BLANCO (10 seg de espera)");
-    esperar(10);
-    calibrar();
-    MIN_WHITE= min;
-    MAX_WHITE=max;
-    
-    if (MAX_BLACK >= MIN_WHITE) { // no se ha calibrado correctamente repetimos proceso
-      Serial.println("No se ha calibrado correctamente, se repetirá el proceso");
-      calibracion=1;
-    } else  {
-      Serial.print("Negro => (");
-      Serial.print (MIN_BLACK);
-      Serial.print (",");
-      Serial.print (MAX_BLACK );
-      Serial.print (")\n");
-      Serial.print("Blanco => (");
-      Serial.print (MIN_WHITE);
-      Serial.print (",");
-      Serial.print (MAX_WHITE);
-      Serial.print (")\n"); 
-      delay(5000);
-      calibracion=0;
-    }        
+int input;
+int i=0;
+void loop() {   
+   for (;i<2;i++) {
+        esperaDato();
+        input = Serial.read(); // Espero señal de calibrar blanco o negro
+        input = input -48;
+        if (input==1) {   
+          digitalWrite (LED_BUILTIN, HIGH);       
+          calibrar();
+          MIN_BLACK= min;
+          MAX_BLACK= max;             
+          serialFlush();                             
+        } else if (input==2) {        
+          digitalWrite (LED_BUILTIN, LOW);
+          calibrar();
+          MIN_WHITE= min;
+          MAX_WHITE= max;                                       
+        }
+        
    }   
-   pulsador = digitalRead(D12);
-   if (pulsador ==HIGH) {     
-     value = analogRead(A0);       
-     if (value >MIN_WHITE && value <MAX_WHITE) 
-       Serial.println("BLANCO");
-     if (value >=MIN_BLACK && value <=MAX_BLACK)
-       Serial.println("NEGRO");  
-     delay(250);  
+   if (i==2) {  
+        if (MAX_BLACK >= MIN_WHITE) { // no se ha calibrado correctamente repetimos proceso
+          Serial.println("CER"); // Avisamos a Netbeans de error de calibracion 
+          i=0;        
+        } else  {
+          Serial.println("OK"); // Calibración correcta  
+          leerColor();        
+        }        
    } 
+     
+  
 }
+
+void leerColor() {
+  while (true) {
+     pulsador = digitalRead(D12);
+     if (pulsador ==HIGH) {     
+       value = analogRead(A0);       
+       if (value >MIN_WHITE && value <MAX_WHITE) 
+         Serial.println("BLANCO");
+       if (value >=MIN_BLACK && value <=MAX_BLACK)
+         Serial.println("NEGRO");  
+       delay(250);  
+      }
+  }
+}
+
+void esperaDato() {
+  while (Serial.available()<=0) {
+    delay(100);
+  }
+}
+
+void serialFlush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+}   
+
 
 void esperar(int t) {
   for (int i=0;i<t;i++) {
@@ -65,7 +81,7 @@ void esperar(int t) {
 }
 
 void calibrar() {
-  Serial.println("Realice circulos sobre el color adecuado y espere mientras tomamos datos");
+  int sw=1;
   for (int i=0;i<5000;i++) {  
     value = analogRead(A0);
     delay(2);
@@ -80,6 +96,6 @@ void calibrar() {
     if (value>max) {
       max =value;
     }    
-    }
+   }
     
 }

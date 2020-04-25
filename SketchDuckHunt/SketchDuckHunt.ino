e7778260d60dda5987a7b6decedecd7603f51281
dperezlledo <dperez.comercio@gmail.com>
2020-04-25 14:39:21 +0200

    Proyecto finalizado!. Ajustes realizados

--- SketchDuckHunt/SketchDuckHunt.ino
+++ SketchDuckHunt/SketchDuckHunt.ino
@@ -1,5 +1,11 @@
 #define D12 12
+#define SWAP(x, y, T) do { T SWAP = x; x = y; y = SWAP; } while (0)
 
+int max, min, pulsador;
+int MIN_BLACK, MAX_BLACK;
+int MIN_WHITE, MAX_WHITE; 
+int input;
+int i=0;
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
@@ -8,11 +14,6 @@ void setup() {
  pinMode(D12, INPUT);
 }
 
-int c=0, max, min,  value, pulsador;
-int MIN_BLACK, MAX_BLACK;
-int MIN_WHITE, MAX_WHITE; 
-int input;
-int i=0;
 void loop() {   
    for (;i<2;i++) {
         esperaDato();
@@ -21,41 +22,69 @@ void loop() {
         if (input==1) {   
           digitalWrite (LED_BUILTIN, HIGH);       
           calibrar();
+          digitalWrite (LED_BUILTIN, LOW);
           MIN_BLACK= min;
           MAX_BLACK= max;             
-          serialFlush();                             
+          serialFlush();  
+          delay(1000);                           
         } else if (input==2) {        
-          digitalWrite (LED_BUILTIN, LOW);
+          digitalWrite (LED_BUILTIN, HIGH);
           calibrar();
+          digitalWrite (LED_BUILTIN, LOW);
           MIN_WHITE= min;
           MAX_WHITE= max;                                       
         }
         
    }   
    if (i==2) {  
-        if (MAX_BLACK >= MIN_WHITE) { // no se ha calibrado correctamente repetimos proceso
-          Serial.println("CER"); // Avisamos a Netbeans de error de calibracion 
+        if (MAX_BLACK+5 >= MIN_WHITE) { // no se ha calibrado correctamente repetimos proceso
+          Serial.println("CER"); // Avisamos a Netbeans de error de calibracion           
           i=0;        
         } else  {
-          Serial.println("OK"); // Calibración correcta  
+          String aux;
+          aux.concat(MIN_BLACK);        
+          aux.concat("-");
+          aux.concat(MAX_BLACK);
+          aux.concat(" ");
+          aux.concat(MIN_WHITE);
+          aux.concat("-");
+          aux.concat(MAX_WHITE);
+          Serial.println(aux); // Calibración correcta  
           leerColor();        
         }        
    } 
-     
-  
 }
 
 void leerColor() {
+  int value=0, erroneos = 0, aciertos=0, fallos=0;
+  int avalues[100];
+  String s1="";
   while (true) {
      pulsador = digitalRead(D12);
-     if (pulsador ==HIGH) {     
-       value = analogRead(A0);       
-       if (value >MIN_WHITE && value <MAX_WHITE) 
-         Serial.println("BLANCO");
-       if (value >=MIN_BLACK && value <=MAX_BLACK)
-         Serial.println("NEGRO");  
-       delay(250);  
-      }
+     if (pulsador == HIGH) {
+       s1.concat(fallos);
+       s1.concat("-");
+       s1.concat(aciertos);
+       s1.concat("-");
+       s1.concat(erroneos);
+       Serial.println(s1); 
+       s1="";     
+       delay(200); // Debemos dar tiempo a Java para cambiar de carta                  
+       for (int i=0;i<100;i++) {
+          avalues[i] = analogRead(A0);
+          delay(2);
+       }
+       bubblesort (avalues, 100); 
+       value = avalues[50];   // Ajustamos entre 0 y 100 al valor más cercano al rango  
+       if (value >= MIN_BLACK && value <= MAX_BLACK) {
+         fallos++; 
+        } else if (value >= MIN_WHITE && value <= MAX_WHITE) {         
+         aciertos++;
+       } else {      
+         erroneos++; 
+       }       
+       Serial.flush();              
+      }    
   }
 }
 
@@ -81,14 +110,15 @@ void esperar(int t) {
 }
 
 void calibrar() {
-  int sw=1;
-  for (int i=0;i<5000;i++) {  
+  int sw=1, value;
+  delay(500);
+  for (int i=0;i<2500;i++) {  
     value = analogRead(A0);
     delay(2);
     if (sw==1) {
-      min=9999;
-      max=-9999;
-      sw =0;
+      min=999;
+      max=-1;
+      sw=0;
     }
     if (value<min) {
       min =value;
@@ -96,6 +126,18 @@ void calibrar() {
     if (value>max) {
       max =value;
     }    
-   }
-    
+   }    
+}
+
+template<typename T> void bubblesort(T *d, size_t s) {
+  bool swapped;
+  do {
+    swapped = false;
+    for (size_t i = 0; i < s-1; i++) {
+      if (d[i] > d[i+1]) {
+        SWAP(d[i], d[i+1], T);
+        swapped = true;
+      }
+    }
+  } while(swapped);
 }
